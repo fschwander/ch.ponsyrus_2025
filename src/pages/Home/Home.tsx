5cb9d590bf57af9bcaea6df968c5532fbe7f234a
fschwander <fschwander@gmx.ch>
2025-04-14 08:21:08 +0200

    first content; styling

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,19 @@
+import House from '/src/assets/images/house.svg?react'
 import './Home.scss'
 
 const Home = () => {
   return (
     <div className={'home'}>
-      <h1>Home</h1>
+      <House className={'home__img-house'} />
+
+      <h1>Ponsyrus Genossenschaft</h1>
+      <p>
+        Anwandstrasse 9, 8004 Zürich <br />
+        <a href='mailto:genossenschaft@ponsyrus.ch'>
+          genossenschaft@ponsyrus.ch
+        </a>
+      </p>
+      <p>Kleingenossenschaftliches Wohnen seit 1984</p>
     </div>
   )
 }
