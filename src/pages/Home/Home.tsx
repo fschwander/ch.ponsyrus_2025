ad7fb1b570fff789a8bbf32dd1e88ce9d603e8b6
fschwander <fschwander@gmx.ch>
2025-04-16 23:34:38 +0200

    sweet first design with animation

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,14 +6,17 @@ const Home = () => {
     <div className={'home'}>
       <House className={'home__img-house'} />
 
-      <h1>Ponsyrus Genossenschaft</h1>
+      <h1 className={'home__title'}>Ponsyrus Genossenschaft</h1>
+
+      <h2>Kontakt</h2>
       <p>
         Anwandstrasse 9, 8004 Zürich <br />
         <a href='mailto:genossenschaft@ponsyrus.ch'>
           genossenschaft@ponsyrus.ch
         </a>
       </p>
-      <p>Kleingenossenschaftliches Wohnen seit 1984</p>
+
+      <p>Kleingenossenschaftliches Wohnen seit 1984.</p>
     </div>
   )
 }
--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -1,13 +1,41 @@
+import { useEffect, useMemo, useRef, useState } from 'react'
 import { Link, Outlet } from 'react-router-dom'
+import useSize from '@react-hook/size'
 import { paths } from '../../routes/routes.tsx'
 import './Layout.scss'
 
 const Layout = () => {
+  const rootRef = useRef<HTMLDivElement>(null)
+
+  const [, containerHeight] = useSize(rootRef)
+  const [scrollY, setScrollY] = useState(0)
+
+  const progress = useMemo(() => {
+    const prog = scrollY / (containerHeight - window.innerHeight)
+    return prog < 0 ? 0 : prog > 1 ? 1 : prog
+  }, [containerHeight, scrollY])
+
+  useEffect(() => {
+    const handleScroll = () => setScrollY(window.scrollY)
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  })
+
+  useEffect(() => {
+    const bodyEl = document.getElementsByTagName('html')
+    const spread = 20
+    const pos = 80
+    const bgColor = `radial-gradient(circle at 50%, transparent ${pos - spread * (1 - progress)}%, var(--clr-secondary) ${pos}%)`
+    if (bodyEl[0]) {
+      bodyEl[0].style.setProperty('--t-body-bg', bgColor)
+    }
+  }, [progress])
+
   return (
-    <div className={'layout'}>
+    <div className={'layout'} ref={rootRef}>
       <nav className={'nav'}>
-        <Link to={paths.HOME}>Home</Link>
-        <Link to={paths.MARKISE}>Markise</Link>
+        <Link to={paths.HOME}>home</Link>
+        <Link to={paths.MARKISE}>markise iii</Link>
       </nav>
 
       <Outlet />
